daeae80d8590a04233818bbe1bb61e5e8cd3761c
Vishal <74290663+dr-cyberx@users.noreply.github.com>
2022-11-06 20:06:52 +0530

    initial commit 0

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: ['plugin:react/recommended', 'standard-with-typescript', 'prettier'],
+	overrides: [],
+	parserOptions: {
+		tsconfigRootDir: __dirname,
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+		project: './tsconfig.json',
+	},
+	plugins: ['react'],
+	settings: {
+		react: {
+			version: 'detect',
+		},
+	},
+	rules: {
+		'react/react-in-jsx-scope': 'off',
+	},
+};
